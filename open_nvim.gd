889501511782b98b4595df2edddefa694dd687de
Degarashi <degarasu@gmail.com>
2025-06-29 14:44:10 +0900

    [update] 並べ替え

--- open_nvim.gd
+++ open_nvim.gd
@@ -1,18 +1,44 @@
 @tool
 extends EditorPlugin
 
+# --------------------------------------------------
+# <Constants>
 const NEOVIM_PATH = "C:/Program Files/neovim/bin/nvim-qt.exe"
 const NEOVIM_OPTIONS = ["-qwindowgeometry", "2048x1200", "--", "--listen", "127.0.0.1:6004"]
-
 const ICON_TEX := preload("res://addons/open_nvim/nvim_logo.png")
+
+# --------------------------------------------------
+# <Private Variable>
 var btn: Button
 var process_id: Array[int] = []
 
 
-static func _is_pid_valid(pid: int) -> bool:
-	return pid != -1
+# --------------------------------------------------
+# <Public Variable>
+# --------------------------------------------------
+# [Private Method (Callback)]
+func _enter_tree() -> void:
+	_prepare_button()
 
 
+func _exit_tree() -> void:
+	for pid in process_id:
+		if _is_pid_valid(pid):
+			OS.execute("taskkill", ["/pid", pid, "/t", "/f"])
+	btn.queue_free()
+
+
+func _on_button_pressed() -> void:
+	var path := _get_script_path_from_sceneroot()
+	var target := "."
+	if not path.is_empty():
+		target = ProjectSettings.globalize_path(path)
+	var options := [target] + NEOVIM_OPTIONS
+	process_id.append(OS.create_process(NEOVIM_PATH, options))
+
+
+# --------------------------------------------------
+# [Private Method]
 func _prepare_button() -> void:
 	btn = Button.new()
 	btn.text = "       Open Nvim"
@@ -31,10 +57,6 @@ func _prepare_button() -> void:
 	add_control_to_container(CONTAINER_TOOLBAR, btn)
 
 
-func _enter_tree() -> void:
-	_prepare_button()
-
-
 func _get_script_path_from_obj(obj: Object) -> String:
 	var script: Script = obj.get_script()
 	if script == null:
@@ -49,17 +71,9 @@ func _get_script_path_from_sceneroot() -> String:
 	return _get_script_path_from_obj(scene_root)
 
 
-func _on_button_pressed() -> void:
-	var path := _get_script_path_from_sceneroot()
-	var target := "."
-	if not path.is_empty():
-		target = ProjectSettings.globalize_path(path)
-	var options := [target] + NEOVIM_OPTIONS
-	process_id.append(OS.create_process(NEOVIM_PATH, options))
-
-
-func _exit_tree() -> void:
-	for pid in process_id:
-		if _is_pid_valid(pid):
-			OS.execute("taskkill", ["/pid", pid, "/t", "/f"])
-	btn.queue_free()
+# --------------------------------------------------
+# [Public Method]
+# --------------------------------------------------
+# [Static Method]
+static func _is_pid_valid(pid: int) -> bool:
+	return pid != -1
